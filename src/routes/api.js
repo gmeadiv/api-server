f7cf5d86f25c4adf19b1462018fdc7c35becdd99
George Mead <gmeadiv@gmail.com>
2021-10-21 14:27:57 -0700

    refactors get all, get one, and post functions

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,60 +1,64 @@
 'use strict';
 
-const { request } = require('express');
 const express = require('express');
 const router = express.Router();
-const { messages, channels} = require('../models');
+const { food, clothes } = require('../models');
 
-const Collection = require('../models/lib/Collection.js')
+const Collection = require('../models/lib/Collection.js');
 
 const modelMap = {
-  messages,
-  channels,
+  food: new Collection(food),
+  clothes: new Collection(clothes),
 };
 
-router.use('/model', (request, response, next) => {
+router.use('/:model', (request, response, next) => {
 
-  const model = modelMap(request.params.model);
-  const method = request.method;
+  const model = modelMap[request.params.model];
+  // const method = request.method;
+
+  if (!model) {
+    next('NO MODEL FOUND');
+  }
 
   request.model = model;
   next();
 
-  switch(method) {
-    case 'GET':
-      model.read(request.params.id);
-      break;
-    case 'POST':
-      model.create(request.body);
-      break;
-    case 'PUT':
-      model.update(request.params.id, request.body);
-      break;
-    case 'DELETE':
-      model.delete(request.params.id);
-      break;
-    default:
-      next('MODEL ROUTER ERROR');
-  }
+  // switch(method) {
+  //   case 'GET':
+  //     model.read(request.params.id);
+  //     break;
+  //   case 'POST':
+  //     model.create(request.body);
+  //     break;
+  //   case 'PUT':
+  //     model.update(request.params.id, request.body);
+  //     break;
+  //   case 'DELETE':
+  //     model.delete(request.params.id);
+  //     break;
+  //   default:
+  //     next('MODEL ROUTER ERROR');
+  // }
+});
 
-  router.get(':/model', async (request, response, next) => {
-    const model = request.model;
-    let records = await model.read();
-    response.send(records);
-  });
+router.get('/:model', async (request, response, next) => {
+  const model = request.model;
+  let records = await model.read();
+  response.send(records);
+});
 
-  router.get(':/model/:id', async (request, response, next) => {
-    const model = request.model;
-    let record = await model.read(request.params.id);
-    response.send(record);
-  });
+router.get('/:model/:id', async (request, response, next) => {
+  const model = request.model;
+  let record = await model.read(request.params.id);
+  response.send(record);
+});
 
-  router.post(':/model', async (request, response, next) => {
-    const model = request.model;
-    const json = request.body;
-    let newRecord = await model.create(json);
-    response.send(newRecord);
-  });
+router.post('/:model', async (request, response, next) => {
+  const model = request.model;
+  const json = request.body;
+  console.log(model, '<-- MODEL | JSON -->', json);
+  let newRecord = await model.create(json);
+  response.send(newRecord);
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -5,48 +5,48 @@ const { food, clothes } = require('./models');
 
 const app = express();
 
-const apirouter = require('./routes/api.js');
+const apiRouter = require('./routes/api.js');
 
 const PORT = process.env.PORT;
 
 const logger = require('./middleware/logger.js');
-const validator = require('./middleware/validator.js');
 const error404 = require('./error-handlers/404.js');
-const error500 = require('./error-handlers/500.js');
-const handleFoodFunctions = require('./routes/food.js');
-const handleClothesFunctions = require('./routes/clothes.js');
+// const validator = require('./middleware/validator.js');
+// const error500 = require('./error-handlers/500.js');
+// const handleFoodFunctions = require('./routes/food.js');
+// const handleClothesFunctions = require('./routes/clothes.js');
 
 app.use(express.json());
 app.use(logger);
+app.use('/api', apiRouter);
 // app.use(validator);
-app.use('/api', apirouter);
 
 // GETS ALL ITEMS
-app.get('/food', async (request, response) => {
-  let recordData = await food.findAll();
-  response.send(recordData);
-});
+// app.get('/food', async (request, response) => {
+//   let recordData = await food.findAll();
+//   response.send(recordData);
+// });
 
-app.get('/clothes', async (request, response) => {
-  let recordData = await clothes.findAll();
-  response.send(recordData);
-});
+// app.get('/clothes', async (request, response) => {
+//   let recordData = await clothes.findAll();
+//   response.send(recordData);
+// });
 
 // GETS ONE SINGLE ITEM
-app.get('/food/:id', handleFoodFunctions.findFood);
-app.get('/clothes/:id', handleClothesFunctions.findClothes);
+// app.get('/food/:id', handleFoodFunctions.findFood);
+// app.get('/clothes/:id', handleClothesFunctions.findClothes);
 
 // CREATES ITEM
-app.post('/food', handleFoodFunctions.postFood);
-app.post('/clothes', handleClothesFunctions.postClothes);
+// app.post('/food', handleFoodFunctions.postFood);
+// app.post('/clothes', handleClothesFunctions.postClothes);
 
 // FINDS AND DELETES ITEM
-app.delete('/food/:id', handleFoodFunctions.deleteFood);
-app.delete('/clothes/:id', handleClothesFunctions.deleteClothes);
+// app.delete('/food/:id', handleFoodFunctions.deleteFood);
+// app.delete('/clothes/:id', handleClothesFunctions.deleteClothes);
 
 // FINDS AND UPDATES ITEM
-app.put('/food/:id', handleFoodFunctions.putFood);
-app.put('/clothes/:id', handleClothesFunctions.putClothes);
+// app.put('/food/:id', handleFoodFunctions.putFood);
+// app.put('/clothes/:id', handleClothesFunctions.putClothes);
 
 app.use(error404);
 // app.use(error500);
